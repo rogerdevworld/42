21abedb1cce0ab2e2999e484da34cda2d6df1da9
Roger Marrero <rmarrero@cbr3s5.42barcelona.com>
2024-09-09 21:32:54 +0200

    24

--- Piscine Reloaded/Exercise/ex22/ft_abs.h
+++ Piscine Reloaded/Exercise/ex22/ft_abs.h
@@ -1,36 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/09 17:57:40 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/09 19:52:35 by rmarrero         ###   ########.fr       */
+/*   Created: 2024/09/09 19:53:06 by rmarrero          #+#    #+#             */
+/*   Updated: 2024/09/09 19:55:49 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef UTILS_H
+#define UTILS_H
 
-void	putchar(char c);
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
-int	ft_strcmp(char *s1, char *s2)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
-	{
-		i++;
-	}
-	return (s1[i] - s2[i]);
-}
-
-int	main(itn agrc, char **argv)
-{
-	int	i;
-	int	j;
-
-	j = 1;
-	if (argc > 1)
-	{
-
-
+#endif
