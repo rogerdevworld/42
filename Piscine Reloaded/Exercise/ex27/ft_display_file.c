a77e6c779ba796c56473e9f5b6e166471df57242
Roger Marrero <rmarrero@car14s6.42barcelona.com>
2024-09-12 05:27:26 +0200

    minasd

--- Piscine Reloaded/Exercise/ex27/ft_display_file.c
+++ Piscine Reloaded/Exercise/ex27/ft_display_file.c
@@ -3,41 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_display_file.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: aybelhaj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/11 15:38:32 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/11 17:28:03 by rmarrero         ###   ########.fr       */
+/*   Created: 2024/09/11 21:26:00 by aybelhaj          #+#    #+#             */
+/*   Updated: 2024/09/11 22:24:38 by aybelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 #include <fcntl.h>
-#define BUF_SIZE 4096
 
 int	main(int argc, char **argv)
 {
 	int		fd;
-	int		read_lines;
-	char	buf[BUF_SIZE + 1];
+	char	buffer[1024];
+	int		bytes_read;
 
-	read_lines = read(fd, buf, BUF_SIZE);
 	if (argc == 1)
 		write(1, "File name missing.\n", 19);
-	else if (argc >= 3)
+	else if (argc > 2)
 		write(1, "Too many arguments.\n", 20);
 	else
 	{
 		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
+		bytes_read = read(fd, buffer, sizeof(buffer));
+		if (fd == -1 || bytes_read == -1)
+			write(1, "Cannot read file.\n", 18);
+		while (bytes_read > 0)
 		{
-			write(1, "Cannot read file.\n", 18);
-			return (1);
+			write(1, buffer, bytes_read);
+			bytes_read = read(fd, buffer, sizeof(buffer));
 		}
-		while ((read_lines = read(fd, buf, BUF_SIZE)) > 0)
-			write(1, buf, read_lines);
-		if (read_lines == -1)
-			write(1, "Cannot read file.\n", 18);
-		if (close(fd) == -1)
-			return (1);
+		close(fd);
 	}
 	return (0);
 }
