62619ab877f4c5fd37f56a92ff03d666b515f2a2
罗杰 - 黄金龙血 <rmarrero@car7s6.42barcelona.com>
2024-09-13 12:31:12 +0200

    other functions

--- Cursus/Circulo - 00/Libft/ft_memcpy.c
+++ Cursus/Circulo - 00/Libft/ft_memcpy.c
@@ -6,20 +6,20 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 06:14:58 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/13 07:22:54 by rmarrero         ###   ########.fr       */
+/*   Updated: 2024/09/13 11:01:43 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	const unsigned char		*local_src;
-	size_t		i;
-	unsigned char	*local_dest;
+	unsigned char		*local_dest;
+	const unsigned char	*local_src;
+	size_t				i;
 
 	i = 0;
-	local_dest = (unsigned	char *)dest;
-	local_src = (const unsigned	char *)src;
+	local_dest = (unsigned char *)dest;
+	local_src = (const unsigned char *)src;
 	while (i < n)
 	{
 		local_dest[i] = local_src[i];
--- Cursus/Circulo - 00/Libft/ft_memmove.c
+++ Cursus/Circulo - 00/Libft/ft_memmove.c
@@ -6,34 +6,31 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 06:48:32 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/13 08:13:57 by rmarrero         ###   ########.fr       */
+/*   Updated: 2024/09/13 12:27:27 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	unsigned char	*local_dest;
+	size_t				i;
+	unsigned char		*local_dest;
 	const unsigned char	*local_src;
 
 	local_dest = (unsigned char *)dest;
-	local_src  = (const unsigned char *)src;
+	local_src = (const unsigned char *)src;
 	if (local_dest == local_src)
 		return (dest);
 	if (local_dest < local_src)
 	{
 		i = 0;
-		while(i < n)
-		{
+		while (i++ < n)
 			local_dest[i] = local_src[i];
-			i++;
-		}
 	}
 	else
 	{
 		i = n;
-		while(i > 0)
+		while (i > 0)
 		{
 			local_dest[i - 1] = local_src[i - 1];
 			i--;
