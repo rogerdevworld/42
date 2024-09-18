018476ccf92ebcc320853fa5112fb10519edb227
罗杰 - 黄金龙血 <rmarrero@car7s6.42barcelona.com>
2024-09-18 19:05:28 +0200

    set

--- Cursus/Circulo - 00/Libft/libft.h
+++ Cursus/Circulo - 00/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 18:01:25 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/18 18:42:52 by rmarrero         ###   ########.fr       */
+/*   Updated: 2024/09/18 18:49:28 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -20,6 +20,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n );
 
 //int funtions
 int		ft_isalpha(int c);
@@ -34,6 +35,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *nptr);
 
 //size_t functions
+size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t	size);
 size_t	ft_strlcat(char *dest, char *src, unsigned int size);
 
