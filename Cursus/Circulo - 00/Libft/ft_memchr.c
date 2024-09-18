15677eb81c15f595bf334aa31751774f925ff82a
罗杰 - 黄金龙血 <rmarrero@car7s6.42barcelona.com>
2024-09-18 18:43:20 +0200

    norminette v2

--- Cursus/Circulo - 00/Libft/ft_memchr.c
+++ Cursus/Circulo - 00/Libft/ft_memchr.c
@@ -6,18 +6,18 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 06:59:23 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/18 07:22:06 by rmarrero         ###   ########.fr       */
+/*   Updated: 2024/09/18 18:41:53 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t				i;
 	const unsigned char	*local_s;
 
 	i = 0;
-	local_s = (const unsigned char	*)s;
+	local_s = (const unsigned char *)s;
 	while (local_s[i])
 	{
 		if (local_s[i] == (unsigned char)c)
@@ -27,4 +27,4 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	if ((unsigned char)local_s[i] == (unsigned char)c)
 		return ((char *)&local_s[i]);
 	return (NULL);
-
+}
--- Cursus/Circulo - 00/Libft/libft.h
+++ Cursus/Circulo - 00/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 18:01:25 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/09/15 17:12:40 by rmarrero         ###   ########.fr       */
+/*   Updated: 2024/09/18 18:42:52 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -21,29 +21,25 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
-
-
 //int funtions
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int     ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
-int	ft_atoi(const char *nptr)
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_atoi(const char *nptr);
 
 //size_t functions
 size_t	ft_strlcpy(char *dest, const char *src, size_t	size);
 size_t	ft_strlcat(char *dest, char *src, unsigned int size);
 
-
 //char functions
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *str, char c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-
 
 #endif
