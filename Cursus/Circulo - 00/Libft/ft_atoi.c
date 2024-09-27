4cb7dc02a14b06183ec3fd46b8a3f2ffabf20ede
罗杰 - 黄金龙血 <rmarrero@car11s3.42barcelona.com>
2024-09-27 18:12:49 +0200

    main

--- Cursus/Circulo - 00/Libft/ft_atoi.c
+++ Cursus/Circulo - 00/Libft/ft_atoi.c
@@ -15,16 +15,16 @@ int	ft_atoi(const char *nptr)
 {
 	int		result;
 	size_t	i;
-	int		sing;
+	int		sign;
 
-	sing = 1;
+	sign = 1;
 	i = 0;
 	result = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '-')
 	{
-		sing = -1;
+		sign = -1;
 		i++;
 	}
 	else if (nptr[i] == '+')
@@ -34,7 +34,7 @@ int	ft_atoi(const char *nptr)
 		result = result * 10 + (nptr[i] - '0');
 		i++;
 	}
-	return (result * sing);
+	return (result * sign);
 }
 /*
 void test_comparison(const char *str) {
