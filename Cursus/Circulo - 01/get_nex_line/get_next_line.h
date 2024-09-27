9ec9833664aaf21b604c846d88000a76a0c4b1c8
罗杰 - 黄金龙血 <rmarrero@car11s3.42barcelona.com>
2024-09-27 23:15:44 +0200

    get_next_line

---
+++ Cursus/Circulo - 01/get_nex_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <rmarrero@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 23:05:39 by rmarrero          #+#    #+#             */
+/*   Updated: 2024/09/27 23:14:06 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* functions of get_next_line_utils.c*/
+
+#endif
